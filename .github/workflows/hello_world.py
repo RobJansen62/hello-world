ff02e067bf0d255f3b225a8d1d14588b8d7ed5b9
Rob Jansen <rob_cj@hotmail.com>
2023-05-21 10:52:23 +0200

    Add files via upload

---
+++ .github/workflows/hello_world.py
@@ -0,0 +1,20 @@
+"""
+Title: Test Python script.
+
+"""
+
+# ----------------------------
+# --- E N T R Y  P O I N T ---
+# ----------------------------
+if (__name__ == "__main__"):
+
+   # Start process
+    print("Hello world!")
+ 
+
+
+
+
+
+
+
